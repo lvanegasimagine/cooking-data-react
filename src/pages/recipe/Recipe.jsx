5c4c13ddbb86626f0a7cc6a2691b2efa05d72306
Luis Vanegas <lvanegas1429@gmail.com>
2022-04-04 15:56:10 -0600

    Button back recipes implementado | useHistory utilizado

--- src/pages/recipe/Recipe.jsx
+++ src/pages/recipe/Recipe.jsx
@@ -1,10 +1,12 @@
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import { useFetch } from "../../hooks/useFetch";
 import { useTheme } from "../../hooks/useTheme";
+import arrowIcon from "../../assets/arrow-back.svg";
 import "./Recipe.css";
 
 const Recipe = () => {
   const { id } = useParams();
+  const history = useHistory();
   const url = `http://localhost:3000/recipes/${id}`;
   const { data: recipe, isPending, error } = useFetch(url);
   const { mode } = useTheme();
@@ -18,12 +20,14 @@ const Recipe = () => {
           <h2 className="page-title">{recipe.title}</h2>
           <p>Takes {recipe.cookingTime} to cook.</p>
           <ul>
-            {recipe.ingredients.map(ing => <li key={ing}>{ing}</li>)}
+            {recipe.ingredients.map((ing) => (
+              <li key={ing}>{ing}</li>
+            ))}
           </ul>
           <p className="method">{recipe.method}</p>
+          <img className="arrowBack" src={arrowIcon} alt="Atras" onClick={history.goBack} />
         </>
-        
-        )}
+      )}
     </div>
   );
 };
